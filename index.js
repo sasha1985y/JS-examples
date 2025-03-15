e2cd70189a0e1d89c7437b6922503a335f9c00bf
sasha1985y <kuyancev1985@mail.ru>
2025-03-15 11:44:44 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,38 @@
+function createDropdown() {
+    // Создаем элемент select
+    var select = document.createElement("select");
+    select.id = "linkSelect";
+    select.onchange = navigate;
+
+    // Создаем опции
+    var options = [
+        { value: "", text: "Выберите ссылку" },
+        { value: "https://sasha1985y.github.io/axios_1/", text: "https://sasha1985y.github.io/axios_1/" },
+        { value: "https://sasha1985y.github.io/axios_get2", text: "axios_get2" },
+        { value: "https://sasha1985y.github.io/axios_post3", text: "axios_post3" },
+        { value: "https://sasha1985y.github.io/axios_config4", text: "axios_config4" },
+        { value: "https://sasha1985y.github.io/axios.params5", text: "axios.params5" },
+        { value: "https://sasha1985y.github.io/axios_post3", text: "axios_post3" },
+    ];
+
+    options.forEach(function(option) {
+        var opt = document.createElement("option");
+        opt.value = option.value;
+        opt.textContent = option.text;
+        select.appendChild(opt);
+    });
+
+    // Добавляем select в body или в нужный контейнер
+    document.body.appendChild(select);
+}
+
+function navigate() {
+    var select = document.getElementById("linkSelect");
+    var url = select.value;
+    if (url) {
+        window.location.href = url; // Переход по выбранной ссылке
+    }
+}
+
+// Вызываем функцию для создания dropdown при загрузке страницы
+window.onload = createDropdown;
